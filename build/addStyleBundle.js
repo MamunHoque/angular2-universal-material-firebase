7a13fe21520651f028c0e1dbb5d3cf8730a6b7b6
leonlovett <1970leonlovett@gmail.com>
2017-09-16 14:58:03 -0500

    added initial firebase configuration

--- build/addStyleBundle.js
+++ build/addStyleBundle.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 var glob = require("glob");
 const index = fs.readFileSync('./src/index.html', 'utf8');
 const path = require('path');
+const firebaseConfig = require('./firebaseConfig.js');
 
 glob('./dist-server/styles.*.bundle.css', { nodir: true }, (err, files) => {    
     const bundleName = files[0].split('/').pop();
     const bundleLink = '<link href="' + bundleName + '" rel="stylesheet">';
-    const serverIndex = index.replace('<!--css-bundle-->', bundleLink);
+    const serverIndex1 = index.replace('<!--css-bundle-->', bundleLink);
+    const serverIndex = serverIndex1.replace('<!--firebase-config-->', firebaseConfig);
     fs.writeFileSync('./dist-server/index.html', serverIndex);
 });
 
---
+++ build/firebaseConfig.js
@@ -0,0 +1,1 @@
+module.exports = `add your firebase config here`;
